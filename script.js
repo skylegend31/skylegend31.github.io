f117acb6e77a9ed13d0805b018e71defbb9bd694
skylegend31 <ilann.oller@epita.fr>
2024-06-18 02:04:53 +0200

    mardi matin

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function afficheMessage() {
     alert("Désolé, ce document n'est pas encore disponible");
   }
-  
+  function afficheMessageBis() {
+    alert("Désolé ce document n'est pas disponible en raison de l'espace de stockage autorisé par Github, notre hébergeur de site. Ce bouton ne marche que en local.")
+  }
